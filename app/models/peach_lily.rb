b8dccbc36ef1cd78fe56398f9cd51ec64527fd9e
Ryan Buckley <arebuckley@gmail.com>
2019-10-07 13:21:36 -0700

    Add all pages for P&L

--- app/models/peach_lily.rb
+++ app/models/peach_lily.rb
@@ -6,12 +6,36 @@ class PeachLily
   def call
     Mechanize.start do |agent|
       create_products(agent, 'Double-Cleanse', 'cleansers')
+      create_products(agent, 'Toners', 'toners')
+      create_products(agent, 'Treatments', 'essence-serum')
+      create_products(agent, 'Treatments', 'serums-ampoules', 'Serum / Ampoule')
+      create_products(agent, 'Facial Oils', 'facial-oils')
+      create_products(agent, 'Eye Care', 'eye-creams', 'Eye Cream')
+      create_products(agent, 'Moisturizers', 'moisturizer', 'Facial Moisturizer')
+      create_products(agent, 'Moisturizers', 'facial-mists', 'Facial Mist')
+      create_products(agent, 'Exfoliators', 'exfoliate', 'Exfoliators')
+      create_products(agent, 'Masks', 'sleeping-masks')
+      create_products(agent, 'Masks', 'masks')
+      create_products(agent, 'Masks', 'sheet-masks')
+      create_products(agent, 'Sun Protection', 'sunscreen')
+      create_products(agent, 'Hair & Beauty', 'body-care')
     end
     true
   end
 
   def create_products(agent, type, key, category = key)
     cat = category.underscore.humanize.titlecase
+    load_paginated_products(agent, key).each do |element|
+      product = Product.new(catalog_id: @catalog.id, product_type: type, category: cat, sourced_from: element.document.url)
+      product.image_url = element.css('.imgcont a > img').first.attr('src')
+      product.name = element.css('.product-title').text
+      product.brand = element.css('.product-vendor').text
+      product.raw_price = element.css('.price .product-price').text
+      product.save || puts(product.errors.full_messages.join(', '))
+    end
+  end
+
+  def load_paginated_products(agent, key)
     products = []
     page_num = 1
     loop do
@@ -22,13 +46,6 @@ class PeachLily
       products.concat(items)
       page_num += 1
     end
-    products.each do |element|
-      product = Product.new(catalog_id: @catalog.id, product_type: type, category: cat, sourced_from: element.document.url)
-      product.image_url = element.css('.imgcont a > img').first.attr('src')
-      product.name = element.css('.product-title').text
-      product.brand = element.css('.product-vendor').text
-      product.raw_price = element.css('.price .product-price').text
-      product.save || puts(product.errors.full_messages.join(', '))
-    end
+    products
   end
 end
