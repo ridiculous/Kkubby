afd99bb5d0af9a17c80e776e784ab39f69073c2c
Ryan Buckley <arebuckley@gmail.com>
2019-10-18 12:16:37 -0700

    Anchor shelfs and products

--- app/controllers/shelf_products_controller.rb
+++ app/controllers/shelf_products_controller.rb
@@ -9,7 +9,7 @@ class ShelfProductsController < ApplicationController
   def create
     @shelf = current_user.shelves.find(params[:shelf_id])
     @shelf.products << product = Product.find(params[:id])
-    redirect_to root_path, notice: "#{product.name} added to your #{@shelf.name}"
+    redirect_to root_path(anchor: product.name.parameterize), notice: "#{product.name} added to your #{@shelf.name}"
   rescue ActiveRecord::RecordNotUnique
     redirect_to product_search_path(shelf_id: params[:shelf_id], query: params[:query]), alert: "Product is already on your shelf"
   end
@@ -19,7 +19,7 @@ class ShelfProductsController < ApplicationController
     @product = @shelf.products.find(params[:id])
     shelf_product = @shelf.shelf_products.where(product_id: params[:id]).first!
     if shelf_product.destroy
-      redirect_to root_path, notice: "#{@product.name} removed from #{@shelf.name}"
+      redirect_to root_path(anchor: @shelf.name.parameterize), notice: "#{@product.name} removed from #{@shelf.name}"
     else
       flash.now[:alert] = "Failed to remove product from shelf"
       render :show
--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -2,7 +2,7 @@ class ShelvesController < ApplicationController
   def create
     shelf = current_user.shelves.new(params.require(:shelf).permit(:name))
     if shelf.save
-      redirect_to root_path, notice: "#{shelf.name} shelf created"
+      redirect_to root_path(anchor: shelf.name.parameterize), notice: "#{shelf.name} shelf created"
     else
       redirect_to root_path, alert: shelf.errors.full_messages.first
     end
