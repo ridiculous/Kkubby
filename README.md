2cb315ef9606bfa1fd9517fafc380cffc9ecfe4f
Ryan Buckley <arebuckley@gmail.com>
2020-08-11 14:46:09 -0700

    Successful deploys

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ App for managing beauty products as shelves, with the ability to share the link 
 
 Setup the server with nginx (https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-18-04)
 Setup the server with RVM (https://github.com/rvm/ubuntu_rvm)
-Setup SSL with letsencrypt
+Setup the server with nodejs and npm (sudo apt-get install nodejs yarn npm && npm install --global yarn) for webpacker
+Setup server with nvm to install correct node JS version (https://phoenixnap.com/kb/update-node-js-version)
+Setup SSL with letsencrypt (https://certbot.eff.org/lets-encrypt/ubuntubionic-nginx)
+Add the example nginx conf (.nginx.conf.example)
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -14,12 +14,12 @@ set :branch, 'master'
 set :rails, lambda { 'bin/rails' }
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'pids', 'sockets')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/master.key')
 # set :force_asset_precompile, true
 
 # For single-user RVM
-set :rvm_path, "/usr/share/rvm/bin/rvm"
-set :rvm_use_path, "/usr/share/rvm/bin/rvm"
+set :rvm_path, "/usr/share/rvm/scripts/rvm"
+set :rvm_use_path, "/usr/share/rvm/scripts/rvm"
 
 set :puma_config, -> { "#{fetch(:current_path)}/config/puma-prod.rb" }
 set :pumactl_socket, -> { "#{fetch(:shared_path)}/sockets/pumactl.sock" }
@@ -28,13 +28,15 @@ set :puma_state, -> { "#{fetch(:shared_path)}/sockets/puma.state" }
 set :puma_pid, -> { "#{fetch(:shared_path)}/pids/puma.pid" }
 
 set :whenever_name, -> { "kkubby_production" }
+set :node_version, 'stable'
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :remote_environment do
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', '2.7.0@default'
-  # command 'source ~/.bashrc'
+  invoke :'rvm:use', '2.7.0@default'
+  invoke :'nvm:load'
+  command 'source ~/.bashrc'
 end
 
 task environment: :remote_environment
@@ -42,6 +44,7 @@ task environment: :remote_environment
 # Put any custom mkdir's in here for when `mina setup` is ran.
 # For Rails apps, we'll make some of the shared paths that are shared between all releases.
 task :setup do
+  invoke :'nvm:install'
   command %[mkdir -p "#{fetch(:shared_path)}/log"]
   command %[chmod g+rx,u+rwx "#{fetch(:shared_path)}/log"]
 
@@ -54,6 +57,7 @@ task :setup do
   command %[mkdir -p "#{fetch(:shared_path)}/config"]
   command %[chmod g+rx,u+rwx "#{fetch(:shared_path)}/config"]
   command %[touch "#{fetch(:shared_path)}/config/database.yml"]
+  command %[touch "#{fetch(:shared_path)}/config/master.key"]
 
   command %[echo "-----> Be sure to edit '#{fetch(:shared_path)}/config/database.yml'."]
 end
@@ -69,40 +73,75 @@ task :deploy do
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
-    # @todo fix for initial setup (currently errors)
-    invoke :'send_pending'
     on :launch do
-      invoke :'whenever:update'
+      # invoke :'whenever:update'
       invoke :'puma:phased_restart'
     end
   end
 end
 
-namespace :whenever do
-  task :clear => :remote_environment do
-    comment "Clear contrab for #{fetch(:whenever_name)}"
-    in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
-    end
+# namespace :whenever do
+#   task :clear => :remote_environment do
+#     comment "Clear contrab for #{fetch(:whenever_name)}"
+#     in_path fetch(:current_path) do
+#       command "#{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+#     end
+#   end
+#
+#   task :update => :remote_environment do
+#     comment "Update crontab for #{fetch(:whenever_name)}"
+#     in_path fetch(:current_path) do
+#       command "#{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+#     end
+#   end
+#
+#   task :write => :remote_environment do
+#     comment "Write crontab for #{fetch(:whenever_name)}"
+#     in_path fetch(:current_path) do
+#       command "#{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+#     end
+#   end
+# end
+
+namespace :nvm do
+  task :load do
+    command 'echo "-----> Loading nvm"'
+    command %{
+      source ~/.nvm/nvm.sh
+    }
+    command 'echo "-----> Now using nvm v.`nvm --version`"'
   end
 
-  task :update => :remote_environment do
-    comment "Update crontab for #{fetch(:whenever_name)}"
-    in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
-    end
-  end
+  task :install do
 
-  task :write => :remote_environment do
-    comment "Write crontab for #{fetch(:whenever_name)}"
-    in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
-    end
-  end
-end
+    # Specifying a Node.js version
+    # copy from: https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version
+    #
+    # Use the engines section of your package.json
+    # {
+    #   "name": "myapp",
+    #   "description": "a really cool app",
+    #   "version": "0.0.1",
+    #   "engines": {
+    #     "node": "0.10.x"
+    #   }
+    # }
 
-task :send_pending do
-  command %[cd #{fetch(:current_path)} ; BUNDLE_GEMFILE=#{fetch(:bundle_gemfile)} RAILS_ENV=production bundle exec rake notifications:send_pending]
+    # package = File.read("package.json")
+    # config = JSON.parse(package)
+    # if config['engines'] && config['engines']['node']
+    #   set :node_version, config['engines']['node']
+    # end
+
+    # Install Node.js via Node Version Manager
+    # and symlink it to project_dir/.bin
+
+    command %{
+      echo "-----> Install node v.#{fetch(:node_version)}"
+      nvm install #{fetch(:node_version)}
+      ln -s ${NVM_BIN} ./.bin
+    }
+  end
 end
 
 task :logs do
