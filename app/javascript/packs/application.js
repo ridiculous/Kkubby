0b011e6f9efa9a14eecb2bd84d8af7e35a105127
Ryan Buckley <arebuckley@gmail.com>
2020-08-11 22:40:13 -0700

    Fixup session store and assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("turbolinks").start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,7 +11,7 @@ require("turbolinks").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-document.addEventListener('turbolinks:load', function () {
+document.addEventListener('DOMContentLoaded', function () {
   var newShelf = document.querySelector('.add-new-shelf')
     , shelfNames = document.querySelectorAll('.shelf-name > span')
     , shelfNameInputs = document.querySelectorAll('.shelf-name input[type=text]')
@@ -54,7 +50,7 @@ document.addEventListener('turbolinks:load', function () {
       console.log('adding event listener')
       element.addEventListener('touchstart', function (event) {
         console.log(event.type)
-      })
+      }, {passive: true})
     });
   }
 });
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,3 @@ Kkubby::Application.config.session_store :cookie_store,
                                          key: 'kkubby_session',
                                          secure: Rails.env.production?,
                                          httponly: true
-Kkubby::Application.config.session_store :cookie_store,
-                                         key: 'auth_token',
-                                         secure: Rails.env.production?,
-                                         httponly: true
