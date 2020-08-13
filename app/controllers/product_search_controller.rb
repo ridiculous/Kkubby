feac99edec464d0bea48970993b3fda27d83161f
Ryan Buckley <arebuckley@gmail.com>
2020-08-13 09:45:01 -0700

    Improvements to the product search

--- app/controllers/product_search_controller.rb
+++ app/controllers/product_search_controller.rb
@@ -1,6 +1,7 @@
 class ProductSearchController < ApplicationController
   before_action :authenticate_user
 
+  # @todo Paginate results
   def index
     @shelf = current_user.shelves.find(params[:shelf_id])
     @products = load_products
@@ -10,7 +11,7 @@ class ProductSearchController < ApplicationController
 
   def load_products
     if params[:query].present?
-      @products = Product.where('name ilike :q OR product_type ilike :q OR brand ilike :q', q: "%#{params[:query]}%").limit(40).order(:name).to_a
+      @products = Product.where('name ilike :q OR product_type ilike :q OR brand ilike :q', q: "%#{params[:query].strip}%").limit(40).order(:name).to_a
     else
       []
     end
