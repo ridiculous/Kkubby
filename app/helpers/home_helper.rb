73c55ae7e24a05b2594d8b90c7296d1cfeba380b
Ryan Buckley <arebuckley@gmail.com>
2020-08-13 20:50:02 -0700

    Update link styling

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,6 +5,8 @@ module HomeHelper
       content_tag(:span, "Did you mean? ") + link_to(result, product_search_path(shelf_id: @shelf.id, query: result))
     elsif query.ends_with?("s")
       content_tag(:span, "Maybe try? ") + link_to(query.singularize, product_search_path(shelf_id: @shelf.id, query: query.singularize))
+    else
+      content_tag(:span, "You could Google ") + link_to(query, "https://google.com/?q=#{query}", target: '_blank')
     end
   end
 end
