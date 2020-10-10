bd7882d43de985b1991e134cdea9d8f31f52fe22
Ryan Buckley <arebuckley@gmail.com>
2020-10-10 14:45:38 -0700

    Update puma config to be bigger with preloading enabled

--- config/puma-prod.rb
+++ config/puma-prod.rb
@@ -1,12 +1,12 @@
 # config/puma.rb
 # currently have one core
-workers 1
+workers 2
 
 # Run GC before fork
 nakayoshi_fork
 
 # Min and Max threads per worker
-threads 1, 8
+threads 1, Integer(ENV['RAILS_MAX_THREADS'] || 5)
 
 shared_dir = "/home/kkubby/shared"
 
@@ -23,6 +23,6 @@ stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.std
 # Set master PID and state locations
 pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
-# preload_app!
+preload_app!
 prune_bundler
 activate_control_app "unix://#{shared_dir}/sockets/pumactl.sock"
