0f6aec1fa84607855b6eef6e6476bbbb56bcabfb
Ryan Buckley <arebuckley@gmail.com>
2020-10-11 15:25:31 -0700

    Fix caching issue with readonly and signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   end
 
   def show
-    @shelves = Rails.cache.fetch @user do
+    @shelves = cache @user do
       @user.shelves.includes(:shelf_products => { :product => { :image_attachment => :blob } }).references(:shelf_products).order('shelves.order_index').to_a
     end
   end
