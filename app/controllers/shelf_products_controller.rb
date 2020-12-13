3c0822870db3b60a3f071834f96a50264e9e88de
Ryan Buckley <arebuckley@gmail.com>
2020-12-13 15:48:46 -0800

    Handle the case when product shelf is not found

--- app/controllers/shelf_products_controller.rb
+++ app/controllers/shelf_products_controller.rb
@@ -1,26 +1,25 @@
 class ShelfProductsController < ApplicationController
   before_action :user_from_params, only: :show
   before_action :authenticate_user, except: :show
-  before_action :load_shelf
 
   def show
-    @shelf_product = @shelf.shelf_products.where(product_id: params[:id]).first!
+    @shelf_product = shelf.shelf_products.where(product_id: params[:id]).first!
     @product = @shelf_product.product
     @catalog = Catalog.find(@product.catalog_id) if @product.catalog_id
-    @title += " - #{@shelf.name} - #{@product.name}"
+    @title += " - #{shelf.name} - #{@product.name}"
   rescue ActiveRecord::RecordNotFound
-    redirect_to home_path
+    redirect_to home_path, alert: 'Resource not found'
   end
 
   def create
-    @shelf.products << @product = Product.find(params[:id])
-    flash[:notice] = "#{@product.name} added to your #{@shelf.name}"
+    shelf.products << @product = Product.find(params[:id])
+    flash[:notice] = "#{@product.name} added to your #{shelf.name}"
   rescue ActiveRecord::RecordNotUnique
     render json: { alert: "Product #{@product.name} is already on your shelf" }, status: :unprocessable_entity
   end
 
   def update
-    @shelf_product = @shelf.shelf_products.where(product_id: params[:id]).first!
+    @shelf_product = shelf.shelf_products.where(product_id: params[:id]).first!
     @product = @shelf_product.product
     if @shelf_product.update_attributes(params.require(:shelf_product).permit(:notes, :wishlist))
       redirect_to home_path(anchor: @product.name.parameterize), notice: "#{@product.name} updated"
@@ -31,10 +30,10 @@ class ShelfProductsController < ApplicationController
   end
 
   def destroy
-    @product = @shelf.products.find(params[:id])
-    shelf_product = @shelf.shelf_products.where(product_id: params[:id]).first!
+    @product = shelf.products.find(params[:id])
+    shelf_product = shelf.shelf_products.where(product_id: params[:id]).first!
     if shelf_product.destroy
-      redirect_to home_path(anchor: @shelf.name.parameterize), notice: "#{@product.name} removed from #{@shelf.name}"
+      redirect_to home_path(anchor: shelf.name.parameterize), notice: "#{@product.name} removed from #{shelf.name}"
     else
       flash.now[:alert] = "Failed to remove product from shelf"
       render :show
@@ -43,7 +42,7 @@ class ShelfProductsController < ApplicationController
 
   private
 
-  def load_shelf
-    @shelf = (@user || current_user).shelves.find(params[:shelf_id])
+  def shelf
+    @shelf ||= (@user || current_user).shelves.find(params[:shelf_id])
   end
 end
